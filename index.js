b838feba7687ec7b5cc328c896a911216d7eb1af
Nishant Bhasin <nishantbhasin@cmail.carleton.ca>
2016-12-02 11:25:48 -0500

    - Code cleanup with better indentation - Added some missing resolvers (url)

--- index.js
+++ index.js
@@ -10,78 +10,93 @@
 module.exports = {
 
     code: {
-            __parseLiteral(ast) {
-                return ast.value;
-            },
-            __parseValue(value) {
-                return value;
-            },
-            __serialize(value) {
-                return value;
-            }
+        __parseLiteral(ast) {
+            return ast.value;
         },
-
-     token: {
-            __parseLiteral(ast) {
-                return ast.value;
-            },
-            __parseValue(value) {
-                return value;
-            },
-            __serialize(value) {
-                return value;
-            }
+        __parseValue(value) {
+            return value;
         },
+        __serialize(value) {
+            return value;
+        }
+    },
 
-     email: {
-            __parseLiteral(ast) {
-                if (ast.kind === Kind.STRING) {
-                    if (validator.isEmail(ast.value)) {
-                        return ast.value;
-                    }
-                }
-                return null;
-            },
-            __parseValue(value) {
-                return value;
-            },
-            __serialize(value) {
-                return value;
-            }
+    token: {
+        __parseLiteral(ast) {
+            return ast.value;
         },
-
-     jwt: {
-            __parseLiteral(ast) {
-                if (ast.kind === Kind.STRING) {
-                    let validator = new JSONSchemaValidator.Validator();
-
-                    // validate payload with schema
-                    if (validator.validate(jwt.verify(ast, config.jwt.secret), jwtPayloadSchema).valid) {
-                        return ast.value;
-                    }
-                }
-                return null;
-            },
-            __parseValue(value) {
-                return value;
-            },
-            __serialize(value) {
-                return value;
-            }
+        __parseValue(value) {
+            return value;
         },
+        __serialize(value) {
+            return value;
+        }
+    },
 
-    password: {
-            __parseLiteral(ast) {
-                if (ast.value.length > 8) {
+    email: {
+        __parseLiteral(ast) {
+            if (ast.kind === Kind.STRING) {
+                if (validator.isEmail(ast.value)) {
                     return ast.value;
                 }
-                return null;
-            },
-            __parseValue(value) {
-                return value;
-            },
-            __serialize(value) {
-                return value
             }
+            return null;
+        },
+        __parseValue(value) {
+            return value;
+        },
+        __serialize(value) {
+            return value;
         }
-    };
+    },
+
+    url: {
+        __parseLiteral(ast) {
+            if (checkUrl(ast.value)) {
+                return ast.value;
+            }
+            return null;
+        },
+        __parseValue(value) {
+            return value;
+        },
+        __serialize(value) {
+            return value
+        }
+    },
+
+    jwt: {
+        __parseLiteral(ast) {
+            if (ast.kind === Kind.STRING) {
+                let validator = new JSONSchemaValidator.Validator();
+
+                // validate payload with schema
+                if (validator.validate(jwt.verify(ast, config.jwt.secret), jwtPayloadSchema).valid) {
+                    return ast.value;
+                }
+            }
+            return null;
+        },
+        __parseValue(value) {
+            return value;
+        },
+        __serialize(value) {
+            return value;
+        }
+    },
+
+    password: {
+        __parseLiteral(ast) {
+            if (ast.value.length > 8) {
+                return ast.value;
+            }
+            return null;
+        },
+        __parseValue(value) {
+            return value;
+        },
+        __serialize(value) {
+            return value
+        }
+    }
+};
